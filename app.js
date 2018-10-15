9c86c8d2afb0c25aaed0cb1c67602b9d69340cd7
Lap Tran <lapth82@gmail.com>
2018-10-15 23:36:05 +0700

    add cors

--- app.js
+++ app.js
@@ -51,4 +51,22 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  var allowedOrigins = ['http://localhost:3000/', 'https://obscure-plateau-83840.herokuapp.com/'];
+  var origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
 module.exports = app;
